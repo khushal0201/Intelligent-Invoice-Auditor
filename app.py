70b9d16d48f793fc0284d7d4975799169ce7da6a
Khushal Makhija <khushal.makhija@ad.infosys.com>
2025-01-12 16:06:04 +0530

    title 3

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 st.set_page_config(
-    page_title="Invoice auditor app",
+    page_title="Intelligent Invoice auditor",
     page_icon="👋",
     initial_sidebar_state="collapsed"
 )
